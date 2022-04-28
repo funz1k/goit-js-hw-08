f85e311adef69bcde5e8bd8634a7cea3f7c7f601
funz1k <92177445+funz1k@users.noreply.github.com>
2022-04-28 22:35:20 +0300

    task-3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,41 @@
+const throttle = require('lodash.throttle');
 
+
+const STORAGE_KEY = 'feedback-form-state';
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    input: document.querySelector('input[name="email"]'),
+    textarea: document.querySelector('textarea[name="message"]'),
+}
+
+const formData = {};
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(onFormData, 500));
+
+populatedTextarea();
+
+function onFormSubmit(e) {
+    e.preventDefault();
+    if (refs.input.value === '' || refs.textarea.value === '') {
+        alert('Все поля формы должны быть заполнены')
+    } else {
+        console.log(formData);
+        e.currentTarget.reset()
+        localStorage.removeItem(STORAGE_KEY)
+    }
+}
+
+function onFormData(e) {
+    formData[e.target.name] = e.target.value
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData))
+}
+
+function populatedTextarea() {
+    const savedMessage = localStorage.getItem(STORAGE_KEY)
+    const parseMessage = JSON.parse(savedMessage)
+    if (parseMessage) {
+        refs.textarea.value = parseMessage.message
+        refs.input.value = parseMessage.email
+    }
+}
