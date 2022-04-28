78b53a49227774dc9650a0751710ec1520cac243
funz1k <92177445+funz1k@users.noreply.github.com>
2022-04-28 21:08:41 +0300

    task-1 and task-2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector('.gallery')
+
+const markup = galleryItems.map(({ preview, original, description }) => {
+    return `
+        <a class="gallery__item" href="${original}">
+    <img
+        class="gallery__image"
+        src="${preview}"
+        alt="${description}"
+    />
+        </a>
+    `
+}).join('')
+
+galleryList.insertAdjacentHTML('afterbegin', markup);
+
+new SimpleLightbox('.gallery a', { overlayOpacity: 0.8, captionsData: 'alt', captionDelay: 250, showCounter: false });
+
+
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import VimeoPlayer from '@vimeo/player';
+const throttle = require('lodash.throttle');
+
+
+let savedData = localStorage.getItem('videoplayer-current-time')
+const parseData = JSON.parse(savedData)
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+player.on('timeupdate', throttle(onLocalRecordTime, 1000))
+
+function onLocalRecordTime(event) {
+    localStorage.setItem('videoplayer-current-time', JSON.stringify(event))
+};
+
+player.setCurrentTime(parseData ? parseData.seconds : 0)
 
