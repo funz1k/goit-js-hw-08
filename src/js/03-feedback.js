ab3205df5d81708e935b5a7182cc5a2ab55a7b7f
funz1k <92177445+funz1k@users.noreply.github.com>
2022-04-29 14:43:47 +0300

    task-3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,13 +2,14 @@ const throttle = require('lodash.throttle');
 
 
 const STORAGE_KEY = 'feedback-form-state';
+const formData = {};
+
+
 const refs = {
     form: document.querySelector('.feedback-form'),
     input: document.querySelector('input[name="email"]'),
     textarea: document.querySelector('textarea[name="message"]'),
 }
-
-const formData = {};
 
 refs.form.addEventListener('submit', onFormSubmit);
 refs.form.addEventListener('input', throttle(onFormData, 500));
@@ -17,25 +18,30 @@ populatedTextarea();
 
 function onFormSubmit(e) {
     e.preventDefault();
-    if (refs.input.value === '' || refs.textarea.value === '') {
-        alert('Все поля формы должны быть заполнены')
-    } else {
-        console.log(formData);
-        e.currentTarget.reset()
-        localStorage.removeItem(STORAGE_KEY)
-    }
+    e.currentTarget.reset()
+    localStorage.removeItem(STORAGE_KEY)
 }
 
 function onFormData(e) {
-    formData[e.target.name] = e.target.value
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData))
+
+    console.log(formData);
+    if (localStorage.STORAGE_KEY) {
+        const parseData = JSON.parse(localStorage.getItem(STORAGE_KEY))
+        formData = { ...parseData };
+        formData[e.target.name] = e.target.value
+        console.log(formData);
+        localStorage.setItem(STORAGE_KEY, JSON.stringify(formData))
+    } else {
+        formData[e.target.name] = e.target.value
+        localStorage.setItem(STORAGE_KEY, JSON.stringify(formData))
+    }
 }
 
 function populatedTextarea() {
     const savedMessage = localStorage.getItem(STORAGE_KEY)
-    const parseMessage = JSON.parse(savedMessage)
-    if (parseMessage) {
-        refs.textarea.value = parseMessage.message
-        refs.input.value = parseMessage.email
+    if (savedMessage) {
+        const parseMessage = JSON.parse(savedMessage)
+        refs.textarea.value = parseMessage.message || ''
+        refs.input.value = parseMessage.email || ''
     }
 }
